56feba472a6012a1c4e7b442a1a01e30f2d2d7b0
SOPHA.RATH <sopha.rath@pnc.lan>
2022-01-28 08:54:22 +0700

    add style

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 ////////////////          start Add Questions        ///////////////////////////////
 //Get value from input
-
-
 let number_of_question = 0;
 function add_question() {
-
     let first_add=false;
     let question_input=document.getElementById('question').value;
     let form_description =document.getElementById('Form description').value;
@@ -14,7 +11,6 @@ function add_question() {
         window.confirm('you need to check correct answer')
     }else{
         first_add=true;
-        // console.log(document.getElementById("question").value)
         number_of_question += 1
     
         let quiz = document.getElementById("quizz")
@@ -40,49 +36,41 @@ function add_question() {
         input_question.className = "question"
         form_group.appendChild(input_question);
     
-            let answers_container = document.createElement("div");
-            answers_container.className = "answers";
-            add_questions_container.appendChild(answers_container);
-        
-            for (let i = 0; i < 4; i++) {
-                let form_group_answer = document.createElement("div");
-                form_group_answer.className = "form-group";
-                answers_container.appendChild(form_group_answer);
-        
-        
-                let radio_answer = document.createElement("INPUT");
-                radio_answer.className = "radio_answer";
-                radio_answer.setAttribute("type", "radio");
-                radio_answer.setAttribute("name", "answer" + number_of_question.toString());
-                form_group_answer.appendChild(radio_answer);
-        
-                let input_answer = document.createElement("INPUT");
-                input_answer.className = "form-control";
-                input_answer.setAttribute("type", "text");
-                input_answer.placeholder = "Answer";
-                form_group_answer.appendChild(input_answer);
-            }
-    }
-
+        let answers_container = document.createElement("div");
+        answers_container.className = "answers";
+        add_questions_container.appendChild(answers_container);
     
+        for (let i = 0; i < 4; i++) {
+            let form_group_answer = document.createElement("div");
+            form_group_answer.className = "form-group";
+            answers_container.appendChild(form_group_answer);
 
-
+            let radio_answer = document.createElement("INPUT");
+            radio_answer.className = "radio_answer";
+            radio_answer.setAttribute("type", "radio");
+            radio_answer.setAttribute("name", "answer" + number_of_question.toString());
+            form_group_answer.appendChild(radio_answer);
+    
+            let input_answer = document.createElement("INPUT");
+            input_answer.className = "form-control";
+            input_answer.setAttribute("type", "text");
+            input_answer.placeholder = "Answer";
+            form_group_answer.appendChild(input_answer);
+        }
+    }
 };
-
-
+// -----------------------alert --------------------------------------------
 let is_display_question = false;
-// -----------------------alert -----------------------------
 let answer_input=document.getElementsByClassName('form-control');
 function display_answer_input(){
     for (let answer of answer_input){
-        if (answer.value===''){
+        if (answer.value ===''){
             return true;
         }
     }
     return false;
 }
-
-
+// ---------------------------check_correct_answer---------------------------
 let radio=document.getElementsByClassName('radio_answer');
 function check_correct_answer(){
     let checked_number = 0;
@@ -90,8 +78,8 @@ function check_correct_answer(){
     let num_0f_q = get_question().length;
     for (let r of radio){
         if (r.checked){
-             checked_number += 1;
-             console.log(checked_number)
+            checked_number += 1;
+            console.log(checked_number)
         }
     }
     if (checked_number === num_0f_q){
@@ -101,13 +89,11 @@ function check_correct_answer(){
     return is_check;
 }
 function display_question() {
-    
-
     let question_input=document.getElementById('question').value;
     let Quizz_Title=document.getElementById('Quizz_Title').value;
     let form_description =document.getElementsByClassName('Form description').value;
 
-    if (question_input==='' || Quizz_Title==='' || form_description==='' || display_answer_input() ) {
+    if (question_input=== "" || Quizz_Title=== "" || form_description=== "" || display_answer_input() ) {
         window.confirm('You must fill all inputs')
     }else if (!(check_correct_answer())){
         window.confirm('you need to check correct answer')
@@ -136,7 +122,6 @@ function display_question() {
             show_input_question.textContent = show_new_question[q];
             show_form_group.appendChild(show_input_question);
         
-        
             let show_answers_container = document.createElement("div");
             show_answers_container.className = "answers";
             show_questions_container.appendChild(show_answers_container);
@@ -144,7 +129,6 @@ function display_question() {
                 let show_form_group_answer = document.createElement("div");
                 show_form_group_answer.className = "form-group";
                 show_answers_container.appendChild(show_form_group_answer);
-        
         
                 let show_radio_answer = document.createElement("div");
                 show_radio_answer.className = "new-radio-answer";
@@ -154,7 +138,6 @@ function display_question() {
                 show_input_answer.className = "form-control";
                 show_input_answer.textContent = show_new_answers[q][j + 1];
                 show_form_group_answer.appendChild(show_input_answer);
-           
             }
             let hide_questons=document.querySelectorAll(".add_questions_container");
             for(let hide of hide_questons){
@@ -162,10 +145,7 @@ function display_question() {
             }
             document.getElementById("btn_add_question").style.display="none";
             document.getElementById("btn_save_question").style.display="none";
-            
-    
         } 
-    
         let hide_questons=document.querySelectorAll(".add_questions_container");
         for(let hide of hide_questons){
             show(hide,false)
@@ -175,34 +155,25 @@ function display_question() {
         is_display_question = true;
         is_display_submit = true;
     }
-};
-
-
+}
+// ---------------------------get_the_question---------------------
 function get_question() {
-
     let add_questions = document.querySelectorAll(".question");
-
     let new_questions = [];
-
     for (let quest of add_questions) {
         new_questions.push(quest.value);
     }
-
     return new_questions
 }
-
-
+// ---------------------------get_the_answers----------------------
 function get_answers(class_name) {
-
     let all_answers = document.getElementsByClassName(class_name)
-
     let new_answers = [];
     let possible_answers = {};
     let number_of_answer = 1;
     let index_answer = 0;
 
     for (let answer of all_answers) {
-
         possible_answers[number_of_answer.toString()] = answer.nextElementSibling.value;
         if (answer.checked) {
             index_answer = number_of_answer;
@@ -212,18 +183,12 @@ function get_answers(class_name) {
             new_answers.push(possible_answers)
             possible_answers = {};
             number_of_answer = 0;
-
         }
         number_of_answer += 1
     }
-    // console.log(new_questions)
- 
     return new_answers
 }
-
-
-// add_question()
-
+// -------------------------show_and_hide---------------------------
 function show(item,isValid){
     if (isValid){
         item.style.display = "block";
@@ -231,87 +196,91 @@ function show(item,isValid){
         item.style.display = "none";
     }
 }
-
-// add_question()
-
+// -------------------------main_btn---------------------------------
 document.getElementById("btn_add_question").addEventListener("click", add_question)
 document.getElementById("btn_save_question").addEventListener("click", display_question)
-
-
 // -------------------------play Quiz--------------------------------
-
+let is_click = false;
 function play_quiz() {
-   if (is_display_question === false) {
-       window.confirm("You need to save the question!")
-       window.style.color = "red";
-   }else { 
-    let play_new_question = get_question();
-    let play_new_answers = get_answers("radio_answer");
-    for (let l = 0; l < play_new_question.length; l++) {
-        number_of_question += 1
-        let play_questions_container = document.createElement("div");
-        play_questions_container.className = "play_answers_container";
-        document.getElementById("quizz").appendChild(play_questions_container);
-
-        let play_questions = document.createElement("div");
-        play_questions.className = "questions";
-        play_questions_container.appendChild(play_questions);
-
-        let play_form_group = document.createElement("div");
-        play_form_group.className = "form-group";
-        play_questions.appendChild(play_form_group);
-
-        let play_input_question = document.createElement("div");
-        play_input_question.className = "form-control";
-        play_input_question.textContent = play_new_question[l];
-        play_form_group.appendChild(play_input_question);
-
-
-        let play_answers_container = document.createElement("div");
-        play_answers_container.className = "answers";
-        play_questions_container.appendChild(play_answers_container);
-        for (let k = 0; k < 4; k++) {
-            let play_form_group_answer = document.createElement("div");
-            play_form_group_answer.className = "form-group";
-            play_answers_container.appendChild(play_form_group_answer);
-
-            let play_radio_answer = document.createElement("INPUT");
-            play_radio_answer.className = "new_radio_answer";
-            play_radio_answer.setAttribute("type", "radio");
-            play_radio_answer.setAttribute("name", "answer"+ number_of_question.toString());
-            play_form_group_answer.appendChild(play_radio_answer);
-
-            let play_input_answer = document.createElement("div");
-            play_input_answer.className = "form-control";
-            play_input_answer.textContent = play_new_answers[l][k + 1];
-            play_form_group_answer.appendChild(play_input_answer);
-            
+    if (is_display_question === false) {
+        if (is_click===false) {
+            window.confirm("You need to save before!")
         }
-    } 
-    let hide_questons=document.querySelectorAll(".add_answers_container");
-        for(let hide of hide_questons){
-            hide.style.display = "none";
-        }
-    document.getElementById("btn_submit_question").style.display = "block";
+    }else { 
+        let play_new_question = get_question();
+        let play_new_answers = get_answers("radio_answer");
+        is_display_question = false;  
+        is_click = true;
+        for (let l = 0; l < play_new_question.length; l++) {
+            number_of_question += 1
+            let play_questions_container = document.createElement("div");
+            play_questions_container.className = "play_answers_container";
+            document.getElementById("quizz").appendChild(play_questions_container);
+
+            let play_questions = document.createElement("div");
+            play_questions.className = "questions";
+            play_questions_container.appendChild(play_questions);
+
+            let play_form_group = document.createElement("div");
+            play_form_group.className = "form-group";
+            play_questions.appendChild(play_form_group);
+
+            let play_input_question = document.createElement("div");
+            play_input_question.className = "form-control";
+            play_input_question.textContent = play_new_question[l];
+            play_form_group.appendChild(play_input_question);
+
+            let play_answers_container = document.createElement("div");
+            play_answers_container.className = "answers";
+            play_questions_container.appendChild(play_answers_container);
+            for (let k = 0; k < 4; k++) {
+                let play_form_group_answer = document.createElement("div");
+                play_form_group_answer.className = "form-group";
+                play_answers_container.appendChild(play_form_group_answer);
+
+                let play_radio_answer = document.createElement("INPUT");
+                play_radio_answer.className = "new_radio_answer";
+                play_radio_answer.setAttribute("type", "radio");
+                play_radio_answer.setAttribute("name", "answer"+ number_of_question.toString());
+                play_form_group_answer.appendChild(play_radio_answer);
+
+                let play_input_answer = document.createElement("div");
+                play_input_answer.className = "form-control";
+                play_input_answer.textContent = play_new_answers[l][k + 1];
+                play_form_group_answer.appendChild(play_input_answer);
+                
+            }
+        } 
+        let hide_questons=document.querySelectorAll(".add_answers_container");
+            for(let hide of hide_questons){
+                hide.style.display = "none";
+            }
+        document.getElementById("btn_submit_question").style.display = "block";
     }
 }
 
-function showResults() {
+// -----------------------------Show_the_result---------------------------
+function show_result() {
     let score = 0
+    
     let old_answer = get_answers("radio_answer");
     let new_answer = get_answers("new_radio_answer")
+    let max_scores = new_answer.length*10;
     for (obj in new_answer){
-        if (old_answer[obj]["correct"] ==new_answer[obj]["correct"]){
-        score += 10
+        if (old_answer[obj]["correct"] === new_answer[obj]["correct"] && new_answer[obj]["correct"]!=0){
+            score += 10
         }
-    }
-  
-    document.getElementById("score").textContent = "scores:" + score;
+    }  
+    document.getElementById("score").style.display = "block" ;
+    let show_scores = document.getElementById("score");
+    show_scores.textContent = "Total scores: " + score + " / " + max_scores;
+    show_scores.style.fontSize = "20px"
+    document.getElementById("btn_submit_question").style.display = "none";
 }
-//-------------------------- mains button----------------------------------
+//-------------------------- mains_button----------------------------------
 document.getElementById("quiz-btn").addEventListener("click", play_quiz);
 document.getElementById("btn_submit_question").style.display = "none";
-document.getElementById("btn_submit_question").addEventListener("click", showResults)
+document.getElementById("btn_submit_question").addEventListener("click", show_result)
 
 
 
@@ -401,7 +370,6 @@ document.getElementById("btn_submit_question").addEventListener("click", showRes
 
 
 
-//---------------------------------------------- End add Question ----------------------------->
 
 
 
@@ -411,7 +379,6 @@ document.getElementById("btn_submit_question").addEventListener("click", showRes
 
 
 
-////////////////////////////////////////// Start Display Question ///////////////////////////////////////
 
 
 
@@ -488,139 +455,3 @@ document.getElementById("btn_submit_question").addEventListener("click", showRes
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ------------------------------------- End of Display Question ------------------------------------>
-
-
-
-
-
-
-
-
-
-/////////////////////////////////////////// Start playing quiz///////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//-------------------------------------------------- End of plying quiz     ----------------------------------------->
-
-//-------------------------------------------------- End of plying quiz     ----------------------------------------->
